7d9745df1550c350e6b63b66008ebd209e57c55a
unknown <matias.pinto.ramirez@gmail.com>
2020-02-22 17:14:48 -0300

    Se continua desarrollando la tabla la cual lista los usuarios, si el usuario esta de cumpleaños muestra un boton para visualizar el poema en una ventana modal, se implementa la paginacion de la tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,23 +9,50 @@ import {AppServiceService} from './app-service.service';
 export class AppComponent implements OnInit{
   constructor(private usuarioService:AppServiceService){}
   private listaUsuarios:Observable<any>;
+  private numerosPagina:Array<number> = new Array<number>();
+  private paginaActiva:number = 0;
+  private cantidadPaginas:number;
+  private isLoading:boolean = false;
   ngOnInit() {
-    this.listarUsuarios(0);
+    this.listarUsuarios(this.paginaActiva);
+    
   }
   title = 'desafioLatam';
 
   listarUsuarios(numeroPagina:number){
+    this.isLoading = true;
     this.usuarioService.listarUsuarios(numeroPagina)
     .subscribe({
       complete:()=>{
-
+        this.isLoading = false;
       },
       next:(response)=>{
+        this.numerosPagina = new Array<number>();
         console.log(response);
+        this.cantidadPaginas = response.totalPages;
+        this.setNumerosPaginas();
+        this.listaUsuarios = response.content;
+        
       },
       error:(response)=>{
         console.log(response);
       }
     });
+  }
+
+  setNumerosPaginas(){
+    for(let i = 0;i<this.cantidadPaginas;i++){
+      this.numerosPagina.push(i);
+    }
+  }
+  cambiarPagina(numeroPagina:number){
+
+    this.paginaActiva = numeroPagina;
+    this.listarUsuarios(numeroPagina);
+  }
+
+  private felicitacionActiva:string = '';
+  verPoema(poema:string){
+    this.felicitacionActiva = poema;
   }
 }
